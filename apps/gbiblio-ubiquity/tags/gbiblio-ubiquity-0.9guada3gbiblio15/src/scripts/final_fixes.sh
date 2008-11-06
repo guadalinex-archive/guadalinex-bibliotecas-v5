8c635a91b4558cbba0780c3cada5f888ee76da5d
juanje <juanje@329f4a31-af52-0410-9944-e3c476395494>
2008-11-06 13:14:07 +0000

    Added script for configuring some stuff after the ubiquity finish. A missed file when the tag was created :-(

---
+++ apps/gbiblio-ubiquity/tags/gbiblio-ubiquity-0.9guada3gbiblio15/src/scripts/final_fixes.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+logs=/var/log/installer/debug
+
+exec > $logs 2>&1
+
+function error() {
+	echo "Error: $*"
+}
+
+cp -v /usr/share/gbiblio-ubiquity/templates/hosts /target/etc/hosts || error "copying hosts"
+
+cp -v /tmp/interfaces /target/etc/network/interfaces  || error "copying interfaces"
+
+if ( grep -q client /proc/cmdline ) ; then
+	line='servidor:/var/nfs/compartido /var/compartido  nfs        rw,hard,intr,auto,user      0 0'
+         echo $line >> /target/etc/fstab || error "Adding nfs line to fstab"
+fi
+
+exit 0
