209f62963b9cdfc9f92257409026945d6b914d88
dteyssiere <dteyssiere@329f4a31-af52-0410-9944-e3c476395494>
2008-11-10 16:51:25 +0000

    adding set of resolv.conf

--- apps/gbiblio-ubiquity/trunk/src/scripts/final_fixes.sh
+++ apps/gbiblio-ubiquity/trunk/src/scripts/final_fixes.sh
@@ -10,6 +10,8 @@ function error() {
 
 cp -v /usr/share/gbiblio-ubiquity/templates/hosts /target/etc/hosts || error "copying hosts"
 
+cp -v /usr/share/gbiblio-ubiquity/templates/resolv.conf /target/etc/resolv.conf || error "copying resolv.conf"
+
 cp -v /tmp/interfaces /target/etc/network/interfaces  || error "copying interfaces"
 
 if ( grep -q client /proc/cmdline ) ; then
