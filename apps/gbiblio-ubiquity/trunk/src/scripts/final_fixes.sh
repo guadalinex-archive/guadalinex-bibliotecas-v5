2348ce4383e0b586176d6424c0a430bba1b44325
juanje <juanje@329f4a31-af52-0410-9944-e3c476395494>
2008-11-12 08:37:44 +0000

    Added checking for the network device on the final script

--- apps/gbiblio-ubiquity/trunk/src/scripts/final_fixes.sh
+++ apps/gbiblio-ubiquity/trunk/src/scripts/final_fixes.sh
@@ -14,6 +14,11 @@ cp -v /usr/share/gbiblio-ubiquity/templates/resolv.conf /target/etc/resolv.conf 
 
 cp -v /tmp/interfaces /target/etc/network/interfaces  || error "copying interfaces"
 
+dev=$(awk -F: '/:/ {gsub(" ","",$1); print $1}' /proc/net/wireless)
+if [ "$dev" != "ath0" ]; then
+        sed -i 's|ath0|'$dev'|g' /target/etc/network/interfaces
+fi
+
 if ( grep -q client /proc/cmdline ) ; then
 	line='servidor:/var/nfs/compartido /var/Compartido  nfs        rw,hard,intr,auto,user      0 0'
          echo $line >> /target/etc/fstab || error "Adding nfs line to fstab"
