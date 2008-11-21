6443e05bc8e23a89c5c3331a53dbef26fcd0a4f7
dteyssiere <dteyssiere@329f4a31-af52-0410-9944-e3c476395494>
2008-11-21 09:59:45 +0000

    adding tag

---
+++ apps/medusa-server/tags/medusa-server-1.19/src/waitingdialog.h
@@ -0,0 +1,50 @@
+/***************************************************************************
+ *   Copyright (C) 2004 by Emergya, S.C.A.                                   *
+ *   info@emergya.info                                                     *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+ 
+ // $Id$
+ // $Author$
+#ifndef WAITINGDIALOG_H
+#define WAITINGDIALOG_H
+
+#include "waitingdialogbase.h"
+
+class QTimer;
+
+/**
+@author Jesús Roncero
+*/
+class WaitingDialog : public WaitingDialogBase
+{
+	Q_OBJECT
+public:
+	WaitingDialog(QWidget *parent = 0, const char *name = 0);
+	void close(int i);
+
+//	~WaitingDialog();
+	
+public slots:
+	void advanceBar();
+	
+private:
+	QTimer *progressTimer;
+
+};
+
+#endif
