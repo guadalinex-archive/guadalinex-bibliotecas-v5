ad68cde217244537f5e29051bc180948bb72f019
jhernandez <jhernandez@329f4a31-af52-0410-9944-e3c476395494>
2008-11-24 17:15:37 +0000

    medusa-client-1.21 tagged

---
+++ apps/medusa-client/tags/medusa-cliente-1.21/src/medusa.h
@@ -0,0 +1,33 @@
+/***************************************************************************
+ *   Copyright (C) 2004 by Junta de Andalucía and Christian Töpp           *
+ *   medusa@juntadeandalucia.es                                            *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef MEDUSA_H
+#define MEDUSA_H
+
+#define MAXMSG 512
+#define BIGBUF 1024
+#define LINEBUFFER 120
+#define PORT "7010"
+#define VERSION 0.03
+#define CONFIGFILE  "/etc/medusa.conf"
+#define FOREGROUND_IMAGE "locked.xpm"
+#define LOGO_IMAGE "logo.xpm"
+
+#endif
