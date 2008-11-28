4bd4dac0fe825c7e712e8988742edad97105b350
dteyssiere <dteyssiere@329f4a31-af52-0410-9944-e3c476395494>
2008-11-28 11:35:46 +0000

    adding tag

---
+++ apps/medusa-server/tags/medusa-server-1.21/src/sendmessagedialog.cpp
@@ -0,0 +1,40 @@
+#include <qtextedit.h>
+#include <qlabel.h>
+#include <qpushbutton.h>
+#include <qvalidator.h>
+
+#include "sendmessagedialog.h"
+
+SendMessageDialog::SendMessageDialog(QWidget *parent, const char *name )
+      : SendMessageDialogBase(parent, name)
+{
+}
+
+void SendMessageDialog::enableSendButton()
+{
+   if (textMessage->length() != 0){
+      sendButton->setEnabled(true);
+      if (textMessage->length() > 120){
+         textMessage->setText(texto);
+         textMessage->moveCursor(QTextEdit::MoveEnd, false);
+      }
+      else{
+         int van = textMessage->length();
+         int quedan = 120 - van;
+         temp.setNum(quedan);
+         charsLabel->setText(temp);
+         texto = textMessage->text();
+      }
+         
+   }
+   else{
+      charsLabel->setText("120");
+      sendButton->setEnabled(false);
+   }
+}
+
+void SendMessageDialog::clearText()
+{
+   textMessage->clear();
+}
+
