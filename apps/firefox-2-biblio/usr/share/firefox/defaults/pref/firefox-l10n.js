51ac90d12419b4664a438560d575c7af342bced5
fontanon <fontanon@329f4a31-af52-0410-9944-e3c476395494>
2009-03-20 10:39:33 +0000

    Se añade driver para RaLink 2400 incluido en paquete de actualización Se añade firefox-2 modificado para AbsysNET

---
+++ apps/firefox-2-biblio/usr/share/firefox/defaults/pref/firefox-l10n.js
@@ -0,0 +1,5 @@
+//@line 36 "/build/buildd/firefox-2.0.0.14+2nobinonly/build-tree/mozilla/browser/locales/../../browser/locales/en-US/firefox-l10n.js"
+
+//@line 38 "/build/buildd/firefox-2.0.0.14+2nobinonly/build-tree/mozilla/browser/locales/../../browser/locales/en-US/firefox-l10n.js"
+
+pref("general.useragent.locale", "en-US");
---
+++ apps/firefox-2-biblio/usr/share/firefox/defaults/pref/vendor.js
@@ -0,0 +1,4 @@
+pref("general.useragent.vendor","Ubuntu");
+pref("general.useragent.vendorSub","8.04");
+pref("general.useragent.vendorComment","hardy");
+pref("browser.display.use_system_colors",   true);
