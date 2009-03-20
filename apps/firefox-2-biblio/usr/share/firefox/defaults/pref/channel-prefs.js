030a87b48244c5d7048c497d17ee0dde2381654d
fontanon <fontanon@329f4a31-af52-0410-9944-e3c476395494>
2009-03-20 10:39:33 +0000

    Se añade driver para RaLink 2400 incluido en paquete de actualización Se añade firefox-2 modificado para AbsysNET

---
+++ apps/firefox-2-biblio/usr/share/firefox/defaults/pref/channel-prefs.js
@@ -0,0 +1,2 @@
+//@line 2 "/build/buildd/firefox-2.0.0.14+2nobinonly/build-tree/mozilla/browser/app/profile/channel-prefs.js"
+pref("app.update.channel", "default");
---
+++ apps/firefox-2-biblio/usr/share/firefox/greprefs/firefox-2.ubuntu-prefs.js
@@ -0,0 +1,4 @@
+
+// only define lock file
+pref("general.config.filename", "firefox-2.cfg");
+
